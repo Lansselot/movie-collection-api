c24df3a252568a43d9860a50e588faf5ade7a2b7
lansselot <pozharov2003@gmail.com>
2025-06-18 21:24:37 +0300

    add auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { userService } from '.';
+import Boom from '@hapi/boom';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import type { AppJwtPayload } from '../types/jwt';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export class AuthService {
+  public async login(email: string, password: string): Promise<string> {
+    const user = await userService.getUserByEmail(email);
+    if (!user) throw Boom.unauthorized('Invalid email or password');
+
+    const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+    if (!isPasswordValid) throw Boom.unauthorized('Invalid email or password');
+
+    const payload: AppJwtPayload = { userId: user.id };
+    const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
+      expiresIn: '1h',
+    });
+
+    return token;
+  }
+}
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,7 @@
+import { AuthService } from './auth.service';
 import { MovieService } from './movie.service';
 import { UserService } from './user.service';
 
 export const userService = new UserService();
 export const movieService = new MovieService();
+export const authService = new AuthService();
